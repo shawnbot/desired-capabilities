80f3488497d7ea64d6eda6a934d93db9dd038697
Shawn Allen <shawn.allen@gsa.gov>
2015-09-21 16:00:35 -0700

    add a bunch of tests

--- test/spec.js
+++ test/spec.js
@@ -1,17 +1,38 @@
 var assert = require('assert');
 var caps = require('../');
 
-describe('parse() (single spec)', function() {
+describe('one()', function() {
 
-  it('parses "ie" as "internet explorer"', function() {
+  it('returns a single capabilities spec', function() {
+    assert.deepEqual(
+      caps.one('ie@8:windows'),
+      {
+        browserName: 'internet explorer',
+        browserVersion: '8',
+        platform: 'windows'
+      }
+    );
+  });
+
+  it('throws an error when it gets more than one', function() {
+    assert.throws(function() {
+      caps.one('ie@8..9');
+    });
+  });
+
+});
+
+describe('string parsing', function() {
+
+  it('treats "ie" as "internet explorer"', function() {
     assert.equal(
-      caps.parse('ie').browserName,
+      caps.one('ie').browserName,
       'internet explorer'
     );
   });
 
-  it('parses versions in the form "name@version"', function() {
-    var c = caps.parse('ie@8')
+  it('recognizes versions in the form "name@version"', function() {
+    var c = caps.one('ie@8')
     assert.equal(c.browserName, 'internet explorer');
     assert.equal(c.browserVersion, 8);
     c = caps.parse('ie@8.0');
@@ -19,14 +40,14 @@ describe('parse() (single spec)', function() {
     assert.equal(c.browserVersion, '8.0');
   });
 
-  it('parses platforms in the form "name:platform"', function() {
-    var c = caps.parse('chrome:windows')
+  it('recognizes platforms in the form "name:platform"', function() {
+    var c = caps.one('chrome:windows')
     assert.equal(c.browserName, 'chrome');
     assert.equal(c.platform, 'windows');
   });
 
-  it('parses version and platform in the form "name@version:platform"', function() {
-    var c = caps.parse('chrome@40:windows')
+  it('recognizes version and platform in the form "name@version:platform"', function() {
+    var c = caps.one('chrome@40:windows')
     assert.equal(c.browserName, 'chrome');
     assert.equal(c.browserVersion, 40);
     assert.equal(c.platform, 'windows');
@@ -34,26 +55,11 @@ describe('parse() (single spec)', function() {
 
 });
 
-describe('getAll()', function() {
-
-  it('parses single browser versions', function() {
-    var c = caps.getAll('ie@8');
-    assert.equal(c.length, 1);
-    assert.deepEqual(
-      c,
-      [
-        {
-          browserName: 'internet explorer',
-          browserVersion: '8',
-          platform: 'any'
-        }
-      ]
-    );
-  });
+describe('version ranges', function() {
 
   it('parses version ranges with ".."', function() {
     assert.deepEqual(
-      caps.getAll('ie@8..9'),
+      caps('ie@8..9'),
       [8, 9].map(function(version) {
         return {
           browserName: 'internet explorer',
@@ -65,7 +71,7 @@ describe('getAll()', function() {
   });
 
   it('parses version ranges with platform: "name@a..b:platform"', function() {
-    var c = caps.getAll('chrome@38..40:windows');
+    var c = caps('chrome@38..40:windows');
     assert.equal(c.length, 3);
     assert.deepEqual(
       c,
@@ -77,27 +83,6 @@ describe('getAll()', function() {
         };
       })
     );
-  });
-
-});
-
-describe('getOne()', function() {
-
-  it('returns a single capabilities spec', function() {
-    assert.deepEqual(
-      caps.getOne('ie@8:windows'),
-      {
-        browserName: 'internet explorer',
-        browserVersion: '8',
-        platform: 'windows'
-      }
-    );
-  });
-
-  it('throws an error when it gets more than one', function() {
-    assert.throws(function() {
-      caps.getOne('ie@8..9');
-    });
   });
 
 });
