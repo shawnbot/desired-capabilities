75d335a707d5d5fd85f0f8c9b20a0e14828a86c0
Shawn Allen <shawn.allen@gsa.gov>
2015-09-21 15:59:53 -0700

    update module structure

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
 var parse = require('./src/parse-string');
 var expand = require('./src/expand');
 
-module.exports = {
-  parse: parse,
-  getAll: getAll,
-  getOne: getOne
-};
-
-function getAll(caps) {
-  if (typeof caps === 'string') {
-    caps = parse(caps);
+function desiredCapabilities(src) {
+  if (typeof src === 'string') {
+    return expand(parse(src));
+  } else if (Array.isArray(src)) {
+    return src.reduce(function(list, d) {
+      return list.concat(desiredCapabilities(d));
+    }, []);
+  } else if (typeof src === 'object') {
+    return expand(src);
   }
-  return expand(caps);
+  throw new Error('Expected string, Array or object; got "' + (typeof src) + '"');
 }
 
-function getOne(caps) {
-  caps = getAll(caps);
+module.exports = desiredCapabilities;
+
+module.exports.parse = parse;
+
+module.exports.expand = expand;
+
+module.exports.one = function getOne(src) {
+  var caps = desiredCapabilities(src);
   if (caps.length > 1) {
-    throw new Error('Got ' + caps.length + ' variations from: ' + JSON.stringify(caps));
+    throw new Error('Got ' + caps.length + ' variations from: ' + JSON.stringify(src));
   }
   return caps[0];
-}
+};
